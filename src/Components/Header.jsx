3f77b397e4556ae585ca76712fef93d821d8f302
aomkar2421 <omkarjagtap1651@gmail.com>
2024-05-16 21:25:16 +0530

    16 May

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="h-14 w-full text-white border-b-2 border-white text-2xl font-bold px-10 flex justify-between items-center " >
+        <span>Filmy <span className="text-red-700  ">Verse</span></span>
+        <h2>Add New</h2>
+    </div>
+  )
+}
+
+export default Header
