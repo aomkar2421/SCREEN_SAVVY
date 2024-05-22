b727af569de970da7cfe57f60ab7d954e44a13a1
aomkar2421 <omkarjagtap1651@gmail.com>
2024-05-22 22:39:55 +0530

    22 May

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -5,6 +5,7 @@ import ReactStars from 'react-stars'
 import { db } from './Firebase/firebase';
 import { getDoc, doc } from 'firebase/firestore';
 import Reviews from './Reviews';
+import { ThreeCircles } from 'react-loader-spinner';
 
 
 const Detail = () => {
@@ -14,36 +15,45 @@ const Detail = () => {
         year : '',
         image : '',
         Description : '',
-        rating : 0
+        rating : 0,
+        rated : 0
     })
+    const [loading, setLoading] = useState(true);
 
     const {id} = useParams();
 
     useEffect( () => {
+        setLoading(true);
         async function getData() {
             const _doc = doc(db, 'movies', id);
             const response = await getDoc(_doc);
             setData(response.data());
         }
         getData();
+        setLoading(false);
     },[] )
 
 
   return (
-    <div className='w-full min-h-screen items-center md:items-start text-white flex flex-col md:flex-row justify-center p-3 mt-4'>
-        <img className='h-96 mr-5 md:sticky top-24 ' src={data.image} alt="" />
-        <div className='md:w-1/2 p-3'>
-        <h1 className='text-3xl font-bold font-sans pb-3 '>{data.title}</h1>
-        <h1 className='text-xl font-bold font-sans pb-3 '>{data.year}</h1>
-            <ReactStars
-            size={20}
-            half={true}
-            value={4.5}
-            edit={false}
-            />
-            <p className='text-lg'>{data.description}</p>
-            <Reviews id={id} />
-        </div>
+    <div>
+        {
+            loading ? <ThreeCircles/> :
+            <div className='w-full min-h-screen items-center md:items-start text-white flex flex-col md:flex-row justify-center p-3 mt-4'>
+                <img className='h-96 mr-5 md:sticky top-24 ' src={data.image} alt="" />
+                <div className='md:w-1/2 p-3'>
+                <h1 className='text-3xl font-bold font-sans pb-3 '>{data.title}</h1>
+                <h1 className='text-xl font-bold font-sans pb-3 '>{data.year}</h1>
+                <ReactStars
+                size={25}
+                half={true}
+                value={data.rating/data.rated}
+                edit={false}
+                />
+                <p className='text-lg'>{data.description}</p>
+                <Reviews id={id} prevRating={data.rating} userRated={data.rated} />
+            </div>
+    </div>
+        }
     </div>
   )
 }
--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { useState } from 'react'
 import ReactStars from 'react-stars'
 import { reviewsRef, db } from './Firebase/firebase'
-import { addDoc, doc, updateDoc } from 'firebase/firestore'
+import { addDoc, doc, updateDoc, query, where, getDocs } from 'firebase/firestore'
 import { TailSpin } from 'react-loader-spinner'
 import swal from 'sweetalert'
+import { useEffect } from 'react'
+import { ThreeDots } from 'react-loader-spinner'
 
 
-const Reviews = ({id}) => {
+const Reviews = ({id, prevRating, userRated}) => {
 
     const [rating, setRating] = useState(0);
     const [loading, setLoading] = useState(false);
+    const [reviewsLoading, setReviewsLoading] = useState(false);
     const [form, setForm] = useState("");
+    const [data, setData] = useState([]);
 
     const sendReview = async () => {
         setLoading(true);
@@ -22,6 +26,12 @@ const Reviews = ({id}) => {
                 rating: rating,
                 thought: form,
                 timestamp: new Date().getTime()
+            })
+
+            const ref = doc(db, 'movies', id);
+            await updateDoc(ref, {
+                rating : prevRating + rating,
+                rated : userRated + 1
             })
 
             setRating(0);
@@ -44,10 +54,28 @@ const Reviews = ({id}) => {
         setLoading(false);
     }
 
+    useEffect( () =>{
+        async function getData(){
+            setReviewsLoading(true);
+
+            let quer = query(reviewsRef, where('movieid' , '==', id))
+            const response = await getDocs(quer);
+            
+            const fetchedData = [];
+            response.forEach((doc) => {
+                fetchedData.push(doc.data());
+            });
+            setData(fetchedData);
+
+            setReviewsLoading(false);
+        }
+        getData();
+    },[] )
+
   return (
     <div className='w-full mt-3 border-t-2' >
         <ReactStars
-            size={35}
+            size={30}
             half={true}
             value={rating}
             onChange={ (rate) => setRating(rate) }
@@ -59,7 +87,34 @@ const Reviews = ({id}) => {
         <button onClick={sendReview} className='w-full flex justify-center items-center mt-2 h-9 bg-green-600 rounded-sm text-white hover:bg-green-800 ' >
             {loading ? <TailSpin height={20} color='white' /> : 'Share' }
         </button>
+
+        {
+            reviewsLoading ? 
+            <div className='mt-6 flex justify-center' > <ThreeDots height={15} color='white' /> </div> 
+            :
+            <div className='mt-4'>
+                {data.map((e, i) => {
+                    return(
+                        <div className=' p-2 w-full border-b header bg-opacity-50 border-gray-600 mt-2' key={i}>
+                            <div className='flex items-center'>
+                                <p className='text-blue-500'>{e.name}</p>
+                                <p className='ml-3 text-xs'>({new Date(e.timestamp).toLocaleString()})</p>
+                            </div>
+                            <ReactStars
+                                size={15}
+                                half={true}
+                                value={e.rating}
+                                edit={false}
+                            />
+
+                            <p>{e.thought}</p>
+                        </div>     
+                    )
+                })}
+            </div>
+            }
     </div>
+
   ) 
 }
 
